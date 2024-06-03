85504caded1aa5bcd00ea818561d0be2202d9081
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-06-03 17:10:29 +0200

    Update 5_Anxiety_Protocol.py

--- pages/5_Anxiety_Protocol.py
+++ pages/5_Anxiety_Protocol.py
@@ -172,6 +172,9 @@ def init_credentials():
 
 def login_page():
     """Login an existing user."""
+    logo_path = "Logo.jpeg"  # Ensure this path is correct relative to your script location
+    st.image(logo_path, use_column_width=True)
+    st.write("---")
     st.title("Login")
     with st.form(key='login_form'):
         username = st.text_input("Username")
@@ -181,6 +184,9 @@ def login_page():
 
 def register_page():
     """Register a new user."""
+    logo_path = "Logo.jpeg"  # Ensure this path is correct relative to your script location
+    st.image(logo_path, use_column_width=True)
+    st.write("---")
     st.title("Register")
     with st.form(key='register_form'):
         new_username = st.text_input("New Username")
