23289f2f8b6d5f5e01e0ae4b37513a54f3186e34
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-06-03 23:11:23 +0200

    Update 5_Anxiety_Protocol.py

--- pages/5_Anxiety_Protocol.py
+++ pages/5_Anxiety_Protocol.py
@@ -189,15 +189,15 @@ def anxiety_protocol():
     st.subheader("What do you think could be the cause?")
     cause = st.text_area("Write your response here", key="cause", height=100)
 
-     # Question 5: Triggers
-    st.subheader("Any specific triggers?")
-    st.write("For example Stress, Caffeine, Lack of Sleep, Social Event, Reminder of traumatic event")
-    triggers = st.text_area("Write your response here", key="triggers", height=100)
-
-    # Question 6: Symptoms
+    # Question 5: Symptoms
     symptoms_list = symptoms_input()
     st.subheader("Add Symptoms")
     other_symptoms = st.text_area("Write your response here", key="other_symptoms", height=10)
+
+    # Question 6: Triggers
+    st.subheader("Any specific triggers?")
+    st.write("For example Stress, Caffeine, Lack of Sleep, Social Event, Reminder of traumatic event")
+    triggers = st.text_area("Write your response here", key="triggers", height=100)
 
     # Question 7: Did something Help
     st.subheader("Did something Help against the Anxiety?")
@@ -211,9 +211,9 @@ def anxiety_protocol():
                 'Location': location,
                 'Anxiety Description': anxiety_description,
                 'Cause': cause,
-                'Triggers': triggers,
                 'Symptoms': symptoms_list,
                 'Add Symptoms': other_symptoms,
+                'Triggers': triggers,
                 'Help': help_response
             }
             new_entry_df = pd.DataFrame([new_entry])
