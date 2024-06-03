9a301ca6a6ecb73c9ef8aae82e0ce96bf3776b90
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-06-03 14:49:52 +0200

    Update 3_Profile.py

--- pages/3_Profile.py
+++ pages/3_Profile.py
@@ -27,6 +27,8 @@ def init_credentials():
             st.session_state.df_users = st.session_state.github.read_df(DATA_FILE)
         else:
             st.session_state.df_users = pd.DataFrame(columns=DATA_COLUMNS)
+        # Ensure phone number columns are treated as strings
+        st.session_state.df_users['emergency_contact_number'] = st.session_state.df_users['emergency_contact_number'].astype(str)
 
 def login_page():
     """ Login an existing user. """
